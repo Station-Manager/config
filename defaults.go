acd653b132a617727bf9d5a7b8826da8c6aa2a5a
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-04 06:57:38 +0200

    Add transaction context timeout and BeginTxContext method
    Introduced `TransactionContextTimeout` in datastore configuration to support transaction-specific timeouts. Implemented `BeginTxContext` in the database service interface to manage transactions with context-based deadlines. Updated default configurations and interfaces accordingly.

--- defaults.go
+++ defaults.go
@@ -7,19 +7,20 @@ import (
 var defaultConfig = types.AppConfig{
 	DatastoreConfigs: []types.DatastoreConfig{
 		{
-			Driver:          "postgres",
-			Host:            "localhost",
-			Port:            5432,
-			Database:        "station_manager",
-			User:            "smuser",
-			Password:        "",
-			SSLMode:         "disable",
-			Debug:           false,
-			MaxOpenConns:    2,
-			MaxIdleConns:    2,
-			ConnMaxLifetime: 10, // Minutes
-			ConnMaxIdleTime: 5,  // Minutes
-			ContextTimeout:  5,  // Seconds
+			Driver:                    "postgres",
+			Host:                      "localhost",
+			Port:                      5432,
+			Database:                  "station_manager",
+			User:                      "smuser",
+			Password:                  "",
+			SSLMode:                   "disable",
+			Debug:                     false,
+			MaxOpenConns:              2,
+			MaxIdleConns:              2,
+			ConnMaxLifetime:           10, // Minutes
+			ConnMaxIdleTime:           5,  // Minutes
+			ContextTimeout:            5,  // Seconds
+			TransactionContextTimeout: 5,  // Seconds
 		},
 	},
 }
