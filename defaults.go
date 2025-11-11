d86f22b0ed4a5e97782f4d10adcb955fb843943b
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2025-11-11 07:28:05 +0200

    Refactor default datastore configuration and bump `types` module to v0.0.8
    Split `DatastoreConfig` into `postgresConfig` and `sqliteConfig` for increased flexibility. Updated `defaultConfig` to use `sqliteConfig` by default. Updated `go.mod` and `go.sum` to reflect the `types` module upgrade to version v0.0.8.

--- defaults.go
+++ defaults.go
@@ -4,25 +4,38 @@ import (
 	"github.com/Station-Manager/types"
 )
 
+var postgresConfig = types.DatastoreConfig{
+	Driver:                    "postgres",
+	Host:                      "localhost",
+	Port:                      5432,
+	Database:                  "station_manager",
+	User:                      "smuser",
+	Password:                  "",
+	SSLMode:                   "disable",
+	Debug:                     false,
+	MaxOpenConns:              2,
+	MaxIdleConns:              2,
+	ConnMaxLifetime:           10, // Minutes
+	ConnMaxIdleTime:           5,  // Minutes
+	ContextTimeout:            5,  // Seconds
+	TransactionContextTimeout: 5,  // Seconds
+}
+
+var sqliteConfig = types.DatastoreConfig{
+	Driver:                    "sqlite",
+	Path:                      "db/data.db",
+	Options:                   map[string]string{"mode": "rwc", "_foreign_keys": "on", "_journal_mode": "WAL", "_busy_timeout": "5000"},
+	Debug:                     false,
+	MaxOpenConns:              2,
+	MaxIdleConns:              2,
+	ConnMaxLifetime:           10, // Minutes
+	ConnMaxIdleTime:           5,  // Minutes
+	ContextTimeout:            5,  // Seconds
+	TransactionContextTimeout: 5,  // Seconds
+}
+
 var defaultConfig = types.AppConfig{
-	DatastoreConfig: types.DatastoreConfig{
-		Driver:                    "postgres",
-		Path:                      "db/data.db",
-		Options:                   "?_busy_timeout=5000&_journal_mode=WAL&_foreign_keys=on&cache=shared&_txlock=immediate",
-		Host:                      "localhost",
-		Port:                      5432,
-		Database:                  "station_manager",
-		User:                      "smuser",
-		Password:                  "",
-		SSLMode:                   "disable",
-		Debug:                     false,
-		MaxOpenConns:              2,
-		MaxIdleConns:              2,
-		ConnMaxLifetime:           10, // Minutes
-		ConnMaxIdleTime:           5,  // Minutes
-		ContextTimeout:            5,  // Seconds
-		TransactionContextTimeout: 5,  // Seconds
-	},
+	DatastoreConfig: sqliteConfig,
 	LoggingConfig: types.LoggingConfig{
 		Level:                  "info",
 		WithTimestamp:          true,
